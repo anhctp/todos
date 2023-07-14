66605ae63840081d92956ac42e8caf972f0a8984
Anh Cao <ctpanh03@gmail.com>
2023-07-14 09:31:28 +0700

    first commit

---
+++ server/todo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from todo.models import Todo
+
+# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+admin.site.register(Todo, TodoAdmin)
